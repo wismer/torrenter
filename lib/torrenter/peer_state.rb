345b4f380dbb0cf8e0a77601104c875133a14bec
wismer <matthewhl@gmail.com>
2014-05-06 13:09:01 -0400

    build up of peer state

--- lib/torrenter/peer_state.rb
+++ lib/torrenter/peer_state.rb
@@ -1,5 +1,4 @@
 module Torrenter
-
   # The buffer state should be responsible ONLY for the handling of non-metadata bytes.
   # the messaging behavior should and ought to remain with the peer class, though
   # in truth, it may be better if I didnt do that. 
@@ -9,9 +8,50 @@ module Torrenter
 
   # instead of initialiazing it with the buffer, 
 
-  class BufferState < Peer
-    def examine(buffer, msg_length)
-      binding.pry
+  class BufferState
+
+    def intialize(socket)
+      @socket = socket
+      @buffer = ''
+    end
+
+    # include Torrenter
+
+    def state(buffer, master_index)
+      buffer_id = buffer[4]
+      if buffer_id.nil?
+        buffer.slice!(0..3)
+      else
+        @length = buffer[0..3].unpack("N>")
+        case buffer_id
+        when BITFIELD   then process_bitfield
+        when HAVE       then process_have
+        when INTERESTED then process_interested
+        when PIECE      then process_piece
+        when CHOKE      then choke_message
+        when HANDSHAKE  then process_handshake
+        end
+      end
+    end
+
+    def process_bitfield
+      
+    end
+
+    def process_have
+      
+    end
+
+    def process_piece
+      
+    end
+
+    def process_handshake
+      
+    end
+
+    def recv_data
+      
     end
   end
 end
